58e39fcf93a128c05bf27aa44fb24c43501810d6
Macedo <filipe@talentprotocol.com>
2025-08-24 08:43:37 +0100

    Update page.tsx

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -31,6 +31,7 @@ import {
 } from "@/lib/constants";
 
 import { Section } from "@/components/common/Section";
+import { PageContainer } from "@/components/common/PageContainer";
 import { Callout } from "@/components/common/Callout";
 import { CalloutCarousel } from "@/components/common/CalloutCarousel";
 import { HandHeart, Gift } from "lucide-react";
